3b63ac27f84b5367e45b9ac035c0a9db91010e4b
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-01-28 23:56:42 +0100

    updated

---
+++ src/components/New.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function New() {
+    return (
+        <div className="new">
+            <div className="sub">
+                <h2>New</h2>
+                <div className="border">
+                    <a>Hydrogen VS Electric Cars</a>
+                    <p>Will hydrogen-fuel cars ever catch up to EVs?</p>
+                </div>
+                <div className="border">
+                    <a>The Downsides of AL Artistry</a>+6
+                    <p>What are the possible adverse effects of on-demand AI image generation?</p>
+                </div>
+                <div>
+                    <a>Is VC Funding Drying Up?</a>
+                    <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default New;
